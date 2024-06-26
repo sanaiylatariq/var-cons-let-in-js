de48ac1970c2be9ae1541b68bebab40b4045d736
sanaiylatariq <sanaiylatariq517@gmail.com>
2024-06-26 22:46:16 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+const accountId = 14453
+let accountEmail = "sunny@gmail.com"
+var accountPassword = "12345"
+accountCity = "multan"
+accountEmail = "kato@gmail.com"
+accountPassword = "334"
+accountCity = "islaabvad"
+
+accountState = "gghrth"
+//accountId = 2
+
+
+
+console.table([accountEmail, accountId, accountPassword, accountState, accountCity])
---
+++ sunny.js
@@ -0,0 +1,6 @@
+let a = 2
+let b = 2
+console.log( a * b)
+let c = 7
+let d = 9
+console.log(c * d);
